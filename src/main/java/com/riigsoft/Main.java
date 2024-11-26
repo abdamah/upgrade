6795fb436de16c611e584603df33b3457b23d93f
Abdallah Mahmoud <javabyabdallah@gmail.com>
2024-11-26 07:48:30 +0300

    Initial commit.

---
+++ src/main/java/com/riigsoft/Main.java
@@ -0,0 +1,12 @@
+package com.riigsoft;
+
+public class Main {
+    public static void main(String[] args) {
+//       DataTypes dt = new DataTypes();
+//       dt.dataTypes();
+
+        Operators op = new Operators();
+        op.operators();
+
+    }
+}
---
+++ src/main/java/com/riigsoft/Operators.java
@@ -0,0 +1,17 @@
+package com.riigsoft;
+
+public class Operators {
+    public void operators() {
+        int a = 3;
+        int b = 5;
+        System.out.println("a == b: " + (b == a));
+        System.out.println("b != a: " + (b != a));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a <= b: " + (a <= b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a < b && b > a: " + (a < b && b > a)); // true
+        System.out.println("a > b && b > a: " + (a > b && b > a)); // false
+        System.out.println("a > b || b > a: " + (a > b || b > a));
+    }
+}
